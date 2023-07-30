ca193935e6ca7dafb039d1d7171bd23bc47c90f8
Lokesh Sharma <kavisthlokesh@gmail.com>
2023-07-30 13:21:36 +0530

    [feat]: loading states

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,57 @@
 import Layout from "./components/Layout";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import TaskList from "./pages/TaskList";
-import CheckList from "./pages/CheckList";
-import CreateTodo from "./pages/CreateTodo";
+import { Suspense, lazy } from "react";
+import Spinner from "./components/Spinner";
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const TaskList = lazy(() => import("./pages/TaskList"));
+const CheckList = lazy(() => import("./pages/CheckList"));
+const CreateTodo = lazy(() => import("./pages/CreateTodo"));
 
 const App = () => {
   return (
     <menu className="bg-gray-900 text-gray-300">
       <Layout>
         <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/" element={<TaskList />} />
-          <Route path="/checklist" element={<CheckList />} />
-          <Route path="/create" element={<CreateTodo />} />
-          <Route path="*" element={<Navigate to={"/"} />} />
+          <Route
+            path="/dashboard"
+            element={
+              <Suspense fallback={<Spinner />}>
+                <Dashboard />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={<Spinner />}>
+                <TaskList />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/checklist"
+            element={
+              <Suspense fallback={<Spinner />}>
+                <CheckList />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/create"
+            element={
+              <Suspense fallback={<Spinner />}>
+                <CreateTodo />
+              </Suspense>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <Suspense fallback={<Spinner />}>
+                <Navigate to={"/"} />
+              </Suspense>
+            }
+          />
         </Routes>
       </Layout>
     </menu>
